566db99f518e9e35635ee59523553c20a7b6507d
Thomas Seidl <remus@gmx.net>
2011-09-17 12:41:42 +0200

    Add an example query plugin for documentation purposes.

---
+++ views/entity_views_example_query.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains an example for a Views query plugin that could use the Entity API Views tables.
+ */
+
+/**
+ * Describes the additional methods looked for on a query plugin by the Entity API.
+ *
+ * Only one of get_result_entities() and get_result_wrappers() needs to be
+ * present, so results can be retrieved. The other methods are optional.
+ */
+abstract class entity_views_axample_query extends views_plugin_query {
+
+  /**
+   * Add a sort to the query.
+   *
+   * This is used to add a sort based on an Entity API property selector instead
+   * of a field alias.
+   *
+   * This method has to be present if click-sorting on fields should be allowed
+   * for some fields using the default Entity API field handlers.
+   *
+   * @param $selector
+   *   The field to sort on, as an Entity API property selector.
+   * @param $order
+   *   The order to sort items in - either 'ASC' or 'DESC'. Defaults to 'ASC'.
+   */
+  public function add_selector_orderby($selector, $order = 'ASC');
+
+  /**
+   * Returns the according entity objects for the given query results.
+   *
+   * @param $results
+   *   The results of the query, as returned by this query plugin.
+   * @param $relationship
+   *   Optionally, a relationship for which the entities should be returned.
+   *
+   * @return
+   *   An array with two entries, keyed by 0 and 1: the entity type of entities
+   *   returned by this method; and the array of entities, keyed by the same
+   *   indexes as the results.
+   */
+  public function get_result_entities($results, $relationship = NULL);
+
+  /**
+   * Returns the according metadata wrappers for the given query results.
+   *
+   * This can be used if no entities for the results can be given, but entity
+   * metadata wrappers can be constructed for them.
+   *
+   * @param $results
+   *   The results of the query, as returned by this query plugin.
+   * @param $relationship
+   *   Optionally, a relationship for which the wrappers should be returned.
+   *
+   * @return
+   *   An array with two entries, keyed by 0 and 1: the entity type of entities
+   *   returned by this method; and the array of EntityMetadataWrapper objects
+   *   representing the results, keyed by the same indexes as the results.
+   */
+  public function get_result_wrappers($results, $relationship = NULL);
+
+}
